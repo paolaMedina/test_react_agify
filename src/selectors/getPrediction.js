79812ef9ef8d656b199786ed218a91bb8b776e41
paola medina <angie.paola.medina@correounivalle.edu.co>
2021-09-10 13:34:47 -0500

    fix:  ignore spaces in name

--- src/selectors/getPrediction.js
+++ src/selectors/getPrediction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getPrediction = async (names, country = '') => {
   try {
-    const stringNames = names.reduce((prev, elem) => `${prev}name[]=${elem}&`, '');
+    const stringNames = names.reduce((prev, elem) => `${prev}name[]=${elem.trim()}&`, '');
     const stringCountry = country !== '' ? `country_id=${country}` : '';
 
     const res = await axios.get(`https://api.agify.io/?${stringNames + stringCountry}`);
